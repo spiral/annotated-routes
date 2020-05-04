e4ebe457d6701ad6d8d22372491abf0a62d4c6ec
Wolfy-J <wolfy.jd@gmail.com>
2020-05-04 13:39:37 +0300

    - first tests

---
+++ tests/Router/RegistryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+declare(strict_types=1);
+
+namespace Spiral\Router\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Core\Container;
+use Spiral\Router\GroupRegistry;
+
+class RegistryTest extends TestCase
+{
+    public function testSameGroup()
+    {
+        $registry = new GroupRegistry(new Container());
+
+        $group = $registry->getGroup('default');
+        $this->assertSame($group, $registry->getGroup('default'));
+    }
+}
